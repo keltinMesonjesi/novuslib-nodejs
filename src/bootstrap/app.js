e31b1cd117a7528536b9b032294ff74161620565
Keltin Mesonjesi <keltin.mesonjesi@protonmail.ch>
2022-12-18 22:25:53 +0100

    Express setup & load env variables

--- src/bootstrap/app.js
+++ src/bootstrap/app.js
@@ -8,3 +8,19 @@
 | the IoC container for the system binding all the various parts.
 |
 */
+const express = require('express');
+const appConfig = require("../config/app");
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(appConfig.port, () => {
+  if (appConfig.env !== 'production') {
+    console.log(`App running on http://localhost:${appConfig.port}`);
+  }
+});
+
+module.exports = app;
--- src/config/app.js
+++ src/config/app.js
@@ -6,3 +6,19 @@
 | Here you will find the basic configurations for your app
 |
 */
+const dotenv = require('dotenv');
+const path = require('path');
+
+// Load env variables from .env file
+dotenv.config({ path: path.join(__dirname, '../.env') });
+
+const envVars = process.env;
+
+module.exports = {
+  name: envVars.APP_NAME,
+  env: envVars.APP_ENV,
+  key: envVars.APP_KEY,
+  debug: envVars.APP_DEBUG,
+  port: envVars.APP_PORT,
+  url: envVars.APP_URL,
+};
